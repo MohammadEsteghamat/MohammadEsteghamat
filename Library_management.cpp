d3de14ad54c808d20959f2d7280ebabe521c6336
MohammadEsteghamat <tradesteghamat@gmail.com>
2024-08-07 03:19:39 +0330

    Update Library_management.cpp end

--- Library_management.cpp
+++ Library_management.cpp
@@ -10,20 +10,34 @@ private:
 	vector<string> authar;
 	vector<int> hire;
 	vector<int> bprrower;
+
 	vector<int> id;
 	vector<int>::iterator it;
 	vector<string>::iterator its;
+	bool __book_hire__(int National_code)
+	{
+		int flg{1};
+		for (size_t i = 0; i < bprrower.size(); i++)
+		{
+			if (bprrower.at(i) == National_code)
+			{
+				flg = 0;
+				cout << "namebook : " << namebook.at(i) << " authar : " << authar.at(i) << endl;
+			}
+		}
+		return flg;
+	}
 
 public:
 	Library_management();
 	void Library_add_member(int National_code);
-	void Library_remove_member(int i);
+	void Library_remove_member(int National_code);
 	void Livrary_add_book(string *name, string *authar1);
 	void Livrary_remove_book(string *name, string *authar1);
-	void borrow(int i, string *name, string *authar1);
-	void book_hire();
-	void printmember(int i);
-	void printid();
+	void book_hire(int National_code);
+	void borrow(int National_code, string *name, string *authar1);
+	void Return_book(int National_code, string *name, string *authar1);
+	void Borrowed_books();
 	~Library_management();
 };
 
@@ -34,36 +48,40 @@ Library_management::Library_management()
 
 void Library_management::Library_add_member(int National_code)
 {
-	for (size_t i = 0; i < id.size(); i += 4)
+	for (size_t i = 0; i < id.size(); i++)
 	{
 		if (id.at(i) == National_code)
 		{
-			cout << "The user with this national code is a member and her username is "
-				 << i + 1000 << endl;
-				 return;
+			cout << "The user with national code " << National_code << " is a member." << endl;
+			return;
 		}
 	}
 
 	id.push_back(National_code);
-	id.push_back(0);
-	id.push_back(0);
-	id.push_back(0);
-	cout << "username:" <<id.size()/4+999<< endl;
+	cout << "username:" << National_code << endl;
 }
 
-void Library_management::Library_remove_member(int b)
+void Library_management::Library_remove_member(int National_code)
 {
-	size_t i;
-	for (i = 0; i < id.size(); i += 4)
+	if (__book_hire__(National_code))
 	{
-		if (id.at(i) == b)
+		for (size_t i = 0; i < id.size(); i++)
 		{
-			break;
+			if (id.at(i) == National_code)
+			{
+				it = id.begin() + i;
+				id.erase(it);
+				cout << "The user with national code " << National_code
+					 << " was deleted." << endl;
+				return;
+			}
 		}
 	}
-
-	it = id.begin() + i;
-	id.erase(it, it + 4);
+	else
+	{
+		cout << "The user with national code " << National_code
+			 << " has not returned the above books." << endl;
+	}
 }
 
 void Library_management::Livrary_add_book(string *name, string *authar1)
@@ -72,77 +90,110 @@ void Library_management::Livrary_add_book(string *name, string *authar1)
 	authar.push_back(*authar1);
 	hire.push_back(0);
 	bprrower.push_back(0);
+	cout << "The book was added to the library." << endl;
 }
 
 void Library_management::Livrary_remove_book(string *name, string *authar1)
 {
-	size_t i;
-	for (i = 0; i < namebook.size(); i++)
+	for (size_t i = 0; i < namebook.size(); i++)
 	{
 		if (namebook.at(i) == *name)
+		{
 			if (authar.at(i) == *authar1)
-				break;
+			{
+				its = namebook.begin() + i;
+				namebook.erase(its);
+				its = authar.begin() + i;
+				authar.erase(its);
+				it = hire.begin() + i;
+				hire.erase(it);
+				it = bprrower.begin() + i;
+				bprrower.erase(it);
+				cout << "The book was removed from the library." << endl;
+				return;
+			}
+		}
 	}
-	it = id.begin() + i;
-	its = namebook.begin() + i;
-	namebook.erase(its);
-	authar.erase(its);
-	hire.erase(it);
-	bprrower.erase(it);
+	cout << "The book was not found." << endl;
 }
 
-void Library_management::borrow(int b, string *name, string *authar1)
+void Library_management::book_hire(int National_code)
 {
-	size_t i;
-	for (i = 0; i < namebook.size(); i++)
+	__book_hire__(National_code);
+}
+
+void Library_management::borrow(int National_code, string *name, string *authar1)
+{
+	for (size_t i = 0; i < namebook.size(); i++)
 	{
 		if (namebook.at(i) == *name)
 			if (authar.at(i) == *authar1)
 			{
-				hire.at(i) = 1;
-				bprrower.at(i) = b;
+				if (hire.at(i) == 0)
+				{
+					hire.at(i) = 1;
+					bprrower.at(i) = National_code;
+					cout << "The book was loaned." << endl;
+				}
+				else
+				{
+					cout << "The book has already been lent." << endl;
+				}
+				return;
 			}
 	}
+	cout << "The book was not found." << endl;
 }
 
-void Library_management::book_hire()
+void Library_management::Return_book(int National_code, string *name, string *authar1)
 {
-	for (size_t i = 0; i < hire.size(); i++)
+	for (size_t i = 0; i < namebook.size(); i++)
 	{
-		if (hire.at(i) == 1)
+		if (namebook.at(i) == *name)
+			if (authar.at(i) == *authar1)
+			{
+				hire.at(i) = 0;
+				bprrower.at(i) = 0;
+				cout << "The book returned to the library." << endl;
+				return;
+			}
+	}
+	cout << "The book was not found." << endl;
+}
+
+void Library_management::Borrowed_books()
+{
+	for (size_t i = 0; i < namebook.size(); i++)
+	{
+		if (hire.at(i) == 0)
 		{
-			cout << "namebook = " << namebook.at(i) << " authar = " << authar.at(i)
-				 << " id = " << bprrower.at(i) << endl;
+			cout << "namebook : " << namebook.at(i) << " authar : " << authar.at(i) << endl;
 		}
 	}
 }
 
-void Library_management::printid()
-{
-	for (size_t i = 0; i < id.size(); i++)
-	{
-		cout << id.at(i) << ' ';
-	}
-}
-
-void Library_management::printmember(int i)
-{
-	if (namebook.size() == 0)
-		return;
-	cout << namebook.at(i) << authar.at(i) << hire.at(i) << bprrower.at(i);
-}
-
 Library_management::~Library_management()
 {
-	cout << "\nfhffhh" << endl;
+	cout << "fhffhh" << endl;
 }
 
 int main()
 {
+	string a = "mohammad";
+	string b = "esteghmat";
 	Library_management o;
 	o.Library_add_member(4812);
-	o.Library_add_member(555);
+	o.Library_add_member(444);
 	o.Library_add_member(4812);
-
+	o.Livrary_add_book(&a, &b);
+	o.Borrowed_books();
+	o.borrow(4812, &a, &b);
+	o.Borrowed_books();
+	o.borrow(444, &a, &b);
+	o.borrow(444, &b, &a);
+	o.book_hire(4812);
+	o.Return_book(4812, &a, &b);
+	o.Library_remove_member(4812);
+	o.Library_remove_member(444);
 	return 0;
 }
